1cfb08805b4ff35a8f18b696e72e2f60b69d040b
Steve-0628 <49326405+Steve-0628@users.noreply.github.com>
2024-12-16 07:15:52 +0000

    fis

--- flake.nix
+++ flake.nix
@@ -1,12 +1,29 @@
 {
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    };
-    outputs = {} : {
-        devShells = pkgs.mkShell {
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell {
             buildInputs = [
-                pkgs.nodejs_20
+              pkgs.nodejs_18
             ];
+          };
         };
-    };
+      }
+    );
 }
